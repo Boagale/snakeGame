a0272a21df274466c5d492865ff289daf4859305
bogale demas <bogidemas@gmail.com>
2024-10-28 01:32:24 +0300

    draw snake body and event controler complete

--- script.js
+++ script.js
@@ -18,3 +18,43 @@ canvas.width = 600;
 canvas.height = 400;
 canvas.style.border = "2px solid gray";
 canvas.style.background = "brown";
+
+setInterval(() => {
+  if (!pause) {
+    //   snakeMoveControler();
+    ctx.clearRect(0, 0, 600, 400);
+
+    // DRAW SNAKE BODY
+    snakeBody.forEach((box) => ctx.fillRect(box[0], box[1], 5, 5));
+
+    //   drawSnakeTarget();
+
+    //   drawScoreText();
+  } else {
+    ctx.fillStyle = "#fff";
+    ctx.font = "24px Courier new";
+    ctx.fillText("Press space button to play?", 50, 200);
+  }
+}, 50);
+
+document.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "ArrowUp":
+      direction = "UP";
+      break;
+    case "ArrowDown":
+      direction = "DOWN";
+      break;
+    case "ArrowRight":
+      direction = "RIGHT";
+      break;
+    case "ArrowLeft":
+      direction = "LEFT";
+      break;
+    case " ":
+      pause = !pause;
+      break;
+    default:
+      break;
+  }
+});
